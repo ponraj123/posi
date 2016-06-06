ea4ef2b021a69d350b620360a41b616ddd6155c1
ponraj123 <ponraj.bluesky@gmail.com>
2016-06-06 21:38:28 +0530

    Create posi.c

---
+++ posi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("The number is positive no:");
+	}
+	else if(n<0)
+	{
+		printf("The number is nagative no:");
+	}
+	else
+	{
+		printf("The number is equal to zero:");
+	}
+	// your code goes here
+	return 0;
+}
